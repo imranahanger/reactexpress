b93e9972bf3ff09edba384d23f9aa396264ad025
imranahanger <imranahanger@gmail.com>
2019-12-07 00:16:20 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+app.get('/api/customers', (req, res) => {
+    const customers = [
+        { id: 1, firstName: 'John', lastName: 'Doe' },
+        { id: 2, firstName: 'Adnan', lastName: 'bhat' },
+        { id: 3, firstName: 'Mohan', lastName: 'lal' }
+    ]
+    res.json(customers)
+})
+const port = 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
